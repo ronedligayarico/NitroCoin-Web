67f1372d9539abcc2ea2b0bdebddb51fca7c8684
Ron <ron@e-bureauet.dk>
2022-01-24 00:12:49 +0800

    save

--- component/sectionOne.tsx
+++ component/sectionOne.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Logo from '../public/images/nitro.png'
+import Image from 'next/image'
 
 const sectionOne = () => {
   return (
@@ -6,6 +8,7 @@ const sectionOne = () => {
     <div className="flex">
       <div className="flex-1">
         <h1 className="text-amber-50 font-logo">NITRO</h1>
+        <Image className="font-logo"  src={Logo} alt="Logo" />
       </div>
     </div>
     </>
