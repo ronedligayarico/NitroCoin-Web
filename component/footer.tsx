46fb598975262e2c910591326824dc512a10e9ec
Ron <ron@e-bureauet.dk>
2022-01-24 01:07:21 +0800

    Save changes

--- component/footer.tsx
+++ component/footer.tsx
@@ -16,11 +16,6 @@ const Footer = () => {
                 <SocialIcon url="https://instagram.com" />
                 <SocialIcon url="https://google.com" />
                 <SocialIcon url="https://telegram.com" />
-                    {/* <a className="social-button facebook" href="https://www.facebook.com/" target="_blank"><i className="fab fa-facebook-f"></i></a>
-                    <a className="social-button twitter" href="https://www.twitter.com/" target="_blank"><i className="fab fa-twitter"></i></a>
-                    <a className="social-button linkedin" href="https://www.linkedin.com/" target="_blank"><i className="fab fa-linkedin"></i></a>
-                    <a className="social-button youtube" href="https://www.youtube.com/" target="_blank"><i className="fab fa-youtube"></i></a>
-                    <a className="social-button instagram" href="https://www.instagram.com/" target="_blank"><i className="fab fa-instagram"></i></a> */}
                 </div>
 
 
