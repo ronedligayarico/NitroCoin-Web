e8745fb285738eab342b2d3cedb0d60a1ab02c4b
alfred <alfred.tupaz@myxlog.com>
2022-01-25 01:19:38 +0800

    buy now link

--- component/navigation.tsx
+++ component/navigation.tsx
@@ -83,7 +83,7 @@ const Navigation = () => {
                             )
                         })
                     }
-                    <button className="px-4  py-2  rounded-full text-white lg:inline-block md:inline-block hidden transition-all duration-300 custom-buy-now">Buy now</button>
+                    <button className="px-4  py-2  rounded-full text-white lg:inline-block md:inline-block hidden transition-all duration-300 custom-buy-now" onClick={() => window.open("https://pancakeswap.finance/swap",'_blank')} >Buy now</button>
                 </ul>
             </div>
             <div className="lg:space-x-6 space-x-1">
