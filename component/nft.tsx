14443eef1f1b9b594482c7155c4374b6cc2d2811
alfred <alfred.tupaz@myxlog.com>
2022-01-26 02:59:07 +0800

    shadows

--- component/nft.tsx
+++ component/nft.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Image from 'next/image'
 import Nft from '../public/images/NFT.png'
+import { motion } from 'framer-motion';
+
 const nft = () => {
   return (
-    <div className="flex lg:flex-row flex-col items-center justify-between gap-8 mb-32">
+    <motion.div 
+    layout
+    animate={{ opacity: 0.5 }}
+    transition={{
+      opacity: { ease: "linear" },
+      layout: { duration: 0.3 }
+    }}
+    className="flex lg:flex-row flex-col items-center justify-between gap-8 mb-32">
         <div className="lg:w-6/12 w-9/12  text-left">
             <div className="w-9/12 mx-auto">
             <h3 className="text-4xl mb-8">NFTs</h3>
@@ -15,7 +24,7 @@ const nft = () => {
         <div  className="w-6/12 ">
              <Image className="font-logo"  src={Nft}alt="Logo" />
         </div>
-    </div>
+    </motion.div>
   );
 };
 
